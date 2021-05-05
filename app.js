e4cde74989a0c170c21989015f220bd7c51226d6
BUSHRA96 <>
2021-05-05 12:57:14 +0000

    push app.js file

---
+++ app.js
@@ -0,0 +1,44 @@
+// start from hear
+ 
+ 'by bushra aljafari'
+var userName ='bushra'; 
+ alert('made by '+userName);
+
+ var x ='my age is ';
+ var y ='2';
+ var s ='4';
+ alert(x+y+s);
+
+ var age=prompt('whats your age?');
+alert('Here is a recommendation for a movie that suits your age')
+
+ if(age>=18){
+ console.log('then you should watch it and this')
+ document.write('<img src="https://m.jwabnet.net/uploads/thumbs/b8c93a617-1.jpg" width="200px">')
+  document.write(
+      '<a href=" https://back.egybest.co/movie/conjuring-the-devil-2020/">this is a link for new released movie </a>')
+
+  }
+else {
+  console.log('this film is not for your age')
+  document.write('<img src="https://m.media-amazon.com/images/M/MV5BMjA1OTUzNTQ5Ml5BMl5BanBnXkFtZTgwODQ4NDkxNjE@._V1_UX182_CR0,0,182,268_AL_.jpg" width="200px">')
+  document.write(
+      '<a href=" https://back.egybest.co/movie/goosebumps-2-haunted-halloween-2018/?ref=search-p1"> Watch Goosebumps</a>')
+
+
+  }
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
